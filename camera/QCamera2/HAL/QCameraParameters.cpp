22e1733bc7d1b9ca8e2fceada297a1acc4c4bd45
nichcream <c.potato23@gmail.com>
2018-04-06 06:47:14 +0800

    santoni: camera: HAL1: Forcefully remove CDS & SW TNR support
    Change-Id: I068354cb71789b58382206aadbb4179e1b4cf314
    Signed-off-by: sweetyicecare <gabrielseedev@outlook.com>

--- camera/QCamera2/HAL/QCameraParameters.cpp
+++ camera/QCamera2/HAL/QCameraParameters.cpp
@@ -13966,13 +13966,6 @@ int32_t QCameraParameters::updatePpFeatureMask(cam_stream_type_t stream_type) {
        feature_mask |= CAM_QCOM_FEATURE_LLVD;
     }
 
-    if (isHighQualityNoiseReductionMode() &&
-            ((stream_type == CAM_STREAM_TYPE_VIDEO) ||
-            (stream_type == CAM_STREAM_TYPE_PREVIEW && getRecordingHintValue() &&
-            isPreviewSeeMoreRequired()))) {
-        feature_mask |= CAM_QTI_FEATURE_SW_TNR;
-    }
-
     // Do not enable feature mask for ZSL/non-ZSL/liveshot except for 4K2k case
     // Enable feature mask for small Jpeg resolutions
     if ((getRecordingHintValue() &&
@@ -14006,20 +13999,6 @@ int32_t QCameraParameters::updatePpFeatureMask(cam_stream_type_t stream_type) {
             ((CAM_STREAM_TYPE_PREVIEW == stream_type) ||
             (CAM_STREAM_TYPE_SNAPSHOT == stream_type))) {
         feature_mask |= CAM_QCOM_FEATURE_EZTUNE;
-    }
-
-    if ((getCDSMode() != CAM_CDS_MODE_OFF) &&
-            ((CAM_STREAM_TYPE_PREVIEW == stream_type) ||
-            (CAM_STREAM_TYPE_VIDEO == stream_type) ||
-            (CAM_STREAM_TYPE_CALLBACK == stream_type) ||
-            (CAM_STREAM_TYPE_POSTVIEW == stream_type) ||
-            ((CAM_STREAM_TYPE_SNAPSHOT == stream_type) &&
-            getRecordingHintValue() && is4k2kVideoResolution()))) {
-         if (m_nMinRequiredPpMask & CAM_QCOM_FEATURE_DSDN) {
-             feature_mask |= CAM_QCOM_FEATURE_DSDN;
-         } else {
-             feature_mask |= CAM_QCOM_FEATURE_CDS;
-         }
     }
 
     if (isTNRSnapshotEnabled() && (CAM_STREAM_TYPE_SNAPSHOT == stream_type)
